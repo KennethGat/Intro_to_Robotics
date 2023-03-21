db1a3aaa8bae3e5b38e728082a408c0f29c80643
KennethGat <kmg8785@uncw.edu>
2023-03-21 08:26:53 +0000

    Added section of interest to filter out background noise, focusing on only the ball

--- Labs/robotics_lab4/scripts/detect_ball.py
+++ Labs/robotics_lab4/scripts/detect_ball.py
@@ -49,8 +49,16 @@ if __name__ == '__main__':
 	rate = rospy.Rate(10)
 
 	while not rospy.is_shutdown():
-		# make sure we process if the camera has started streaming images
+		# Process only on receipt of camera images
 		if img_received:
+		
+			# Define a numpy array with same dimensions as image being processed
+			# ....for purposes of reducing background noise
+			rectangle = np.zeros ((720, 1280), dtype = "uint8")
+			
+			# Define a region of interest in the array above ('Crop' function)
+			cv2.rectangle(rectangle, (300, 210), (700, 500), [255, 255, 255],  -1)
+			
 			# Filter the Image using HSV Color Space			
 			hsv_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2HSV)
 			
@@ -59,10 +67,16 @@ if __name__ == '__main__':
 			upper_yellow_hsv = np.array([60,255,255])
 			
 			# Filter the image to make the yellow ball stand out
-			hsv_img_filtered = cv2.inRange(hsv_img, lower_yellow_hsv, upper_yellow_hsv)
+			hsv_img = cv2.inRange(hsv_img, lower_yellow_hsv, upper_yellow_hsv)
+			
+			# Use bitwiseAND function so that only the select image region is processed
+			hsv_img = cv2.bitwise_and(hsv_img, rectangle)
+			
+			# Convert the image to monochannel (still trying to figure this one out)
+			# hue, saturation, intensity = cv2.split(hsv_img)
 			
 			# convert it to ros msg and publish it
-			img_msg = CvBridge().cv2_to_imgmsg(hsv_img_filtered, encoding="mono8")
+			img_msg = CvBridge().cv2_to_imgmsg(hsv_img, encoding="mono8")
 			
 			# publish the image
 			img_pub.publish(img_msg)
