ca2e9c68fc90421ce4e511faa2ddf75a88d953d1
KennethGat <kmg8785@uncw.edu>
2023-04-27 06:36:04 +0000

    Added nodes needed for tennis ball pick up and drop task

---
+++ Phase_Reports/Phase_2/Packages/phase2_report/scripts/manual_initialization_kenneth_1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import rospy
+import math
+# import the messages for reading the joint positions and sending joint commands
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from std_msgs.msg import Header
+
+
+if __name__ == '__main__':
+	# initialize the node
+	rospy.init_node('manual_initialization', anonymous = True)
+	# add a publisher for sending joint position commands
+	pos_pub = rospy.Publisher('/pos_joint_traj_controller/command', JointTrajectory, queue_size = 10)
+	# set a 10Hz frequency for this loop
+	loop_rate = rospy.Rate(10)
+
+	# define a joint trajectory variable for sending the control commands
+	pos_cmd = JointTrajectory()
+	pos_cmd_point = JointTrajectoryPoint()
+	# just a quick solution to complete the message template
+	pos_cmd.joint_names.append('elbow_joint')
+	pos_cmd.joint_names.append('shoulder_lift_joint')
+	pos_cmd.joint_names.append('shoulder_pan_joint')
+	pos_cmd.joint_names.append('wrist_1_joint')
+	pos_cmd.joint_names.append('wrist_2_joint')
+	pos_cmd.joint_names.append('wrist_3_joint')
+	
+	# initialize the position command to zero
+	for joint_no in range(6):
+		pos_cmd_point.positions.append(0.0)
+	# set the ideal time to destination
+	pos_cmd_point.time_from_start = rospy.Duration(1.0) # here one second 
+	
+	# change the value of the shoulder_lift_joint to mimic the report's requirement
+	pos_cmd_point.positions[1] = -1.88
+	# change the value of the elbow_joint to mimic the report's requirement
+	pos_cmd_point.positions[0] = 1.57
+	# change the value of the shoulder_pan_joint to mimic the report's requirement
+	pos_cmd_point.positions[2] = 0.63
+	# change the value of the wrist_1_joint to mimic the report's requirement
+	pos_cmd_point.positions[3] = -2.01
+	# change the value of the wrist_2_joint to mimic the report's requirement
+	pos_cmd_point.positions[4] = -1.63
+	
+	# add the trajectory point to the command
+	pos_cmd.points.append(pos_cmd_point)
+	# define a message header	
+	header = Header()
+	
+	while not rospy.is_shutdown():
+		# update the header with the most recent time stamp
+		header.stamp = rospy.Time.now()
+		# use the most recent header in the position command message
+		pos_cmd.header = header
+		# publish the message
+		pos_pub.publish(pos_cmd)
+		# wait for 0.1 seconds until the next loop and repeat
+		loop_rate.sleep()
--- Phase_Reports/Phase_2/Packages/phase2_report/scripts/ros_tf2_with_planner_kenneth.py
+++ Phase_Reports/Phase_2/Packages/phase2_report/scripts/ros_tf2_with_planner_kenneth.py
@@ -23,6 +23,10 @@ from geometry_msgs.msg import Quaternion
 import tf2_geometry_msgs
 from robot_vision_lectures.msg import SphereParams
 
+# import the plan message
+from ur5e_control.msg import Plan
+from geometry_msgs.msg import Twist
+
 
 '''
 ===================================================================
@@ -93,6 +97,9 @@ if __name__ == '__main__':
 	# initialize the node
 	rospy.init_node('ros_tf2_kenneth', anonymous = True)
 	
+	# add a publisher for sending joint position commands
+	plan_pub = rospy.Publisher('/plan', Plan, queue_size = 10)
+	
 	# add a subscriber to the /sphere_params topic to ......
 	# get the estimated tennis ball parameters for use in frame calculations
 	rospy.Subscriber('/sphere_params', SphereParams, call_back)
@@ -153,6 +160,72 @@ if __name__ == '__main__':
 		# Print the resultant ball camera frame with respect to the robot base frame
 		print('Transformed ball camera point in the BASE frame:  x= ', format(pt_in_base.point.x, '.3f'), '(m), y= ', format(pt_in_base.point.y, '.3f'), '(m), z= ', format(pt_in_base.point.z, '.3f'),'(m)')
 		
+		# define a plan variable
+		plan = Plan()
+		
+		# Implement the planner
+		plan_point1 = Twist()
+		# define a point near pickup position
+		plan_point1.linear.x = -0.01459
+		plan_point1.linear.y = -0.4771
+		plan_point1.linear.z = 0.58211
+		plan_point1.angular.x = -2.58878
+		plan_point1.angular.y = -0.0504
+		plan_point1.angular.z = 2.80972
+		# add this point to the plan
+		plan.points.append(plan_point1)
+		
+		plan_point2 = Twist()
+		# define a point for the pickup position
+		# x, y & z co-ordinates referenced from the 
+		# derived sphere fit frame w.r.t. ur5e base frame
+		plan_point2.linear.x = pt_in_base.point.x
+		plan_point2.linear.y = pt_in_base.point.y
+		plan_point2.linear.z = pt_in_base.point.z + 0.33690127
+		plan_point2.angular.x = -3.0506
+		plan_point2.angular.y = -0.059
+		plan_point2.angular.z = 2.806
+		# add this point to the plan
+		plan.points.append(plan_point2)
+		
+		plan_point3 = Twist()
+		# define a point for a safe position above 'place' point 
+		plan_point3.linear.x = -0.5438
+		plan_point3.linear.y = 0.3359
+		plan_point3.linear.z = 0.4952
+		plan_point3.angular.x = -2.7618
+		plan_point3.angular.y = -0.05499
+		plan_point3.angular.z = 0.794875
+		# add this point to the plan
+		plan.points.append(plan_point3)
+		
+		
+		plan_point4 = Twist()
+		# Final 'place/drop' position definition
+		plan_point4.linear.x = -0.579
+		plan_point4.linear.y = 0.369
+		plan_point4.linear.z = 0.2989
+		plan_point4.angular.x = -3.0764
+		plan_point4.angular.y = -0.0591
+		plan_point4.angular.z = 0.813
+		# add this point to the plan
+		plan.points.append(plan_point4)
+		
+		
+		plan_point5 = Twist()
+		# Back to the safe initial position as defined in the
+		# manual_initialization module
+		plan_point5.linear.x = -0.234489
+		plan_point5.linear.y = -0.32785
+		plan_point5.linear.z = 0.68358
+		plan_point5.angular.x = -2.3874
+		plan_point5.angular.y = -0.04317
+		plan_point5.angular.z = 2.1586
+		# add this point to the plan
+		plan.points.append(plan_point5)
+		
+		# publish the plan
+		plan_pub.publish(plan)		
 		
 		# Pause till the next iteration
 		loop_rate.sleep()
